8e46e355e476e3f05aac9772794823c7e28a7626
Yebom <92360412+hb200354@users.noreply.github.com>
2025-09-22 16:34:00 +0900

    update

--- d01_database/a01_select.sql
+++ d01_database/a01_select.sql
@@ -13,3 +13,32 @@ SELECT DNAME, LOC FROM DEPT;
 -- EX3) SALGRADE 테이블에 컬럼 2개를 로딩 하는 명령어 실행SELECT * FROM EMP;
 SELECT * FROM SALGRADE;
 SELECT GRADE, LOSAL FROM SALGRADE;
+
+/*
+# 컬럼의 별칭 사용하기
+1. 모든 테이블의 구성요소 컬럼은 SELECT 컬럼명으로 사용하여 호출할 수 있다.
+2. 그러나, 특정한 경우에는 컬럼명을 변경하거나, 통합된 컬럼명으로 처리해야하는 경우가 있다. 
+EX) SELECT ENAME AS NAME 
+==> 원래 테이블에서의 컬럼명은 ENAME이지만, 데이터 로딩하여 출력할 때는 NAME으로 설정하여 처리
+EX) SELECT ENAME || DEPTNO ENAMEDEPT
+==> ENAME 컬럼과 DEPTNO 컬럼을 통합하여 하나의 컬럼으로 처리
+EX) SELECT SAL * 100 SALMULTI 
+==> 원래는 SAL 컬럼이지만, 100을 곱한 결과를 SALMULTI라는 이름으로 처리
+3. 기본 형식S
+SELECT 컬럼명 AS 별칭 AS : ALIAS(별칭)
+       컬럼명 별칭 : 공백으로 별칭을 바로 사용 
+*/
+SELECT EMPNO AS NO, ENAME NAME 
+FROM EMP;
+-- 데이터 1 || 데이터 2 데이터와 데이터를 연결시 사용하는 키워드
+-- ' - ' : 데이터베이스에서 문자열 데이터를 표현할 때 사용
+SELECT ENAME, DEPTNO, ENAME || ' - ' || DEPTNO DEPTNO_ENAME
+FROM EMP;
+SELECT ENAME, SAL, SAL * 100 SALMULTI
+FROM EMP;
+-- EX1) EMP 테이블에 SAL ==> SALARY 표현, DEPTNO ==> DNO라고 표현
+SELECT ENAME, SAL AS SALARY, DEPTNO DNO
+FROM EMP;
+-- EX2) EMPNO, ENAME, SAL와 급여의 50%(SAL50PER)를 출력하세요
+SELECT EMPNO, ENAME, SAL, SAL * 0.5 SAL50PER
+FROM EMP;
